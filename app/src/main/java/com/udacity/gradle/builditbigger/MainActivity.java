1fad541d41f0b2492e3b6d53da4421ac15a8d4d7
jon <newbluetwo@gmail.com>
2016-03-29 10:02:32 -0400

    move AsyncTask to its own file in preparation for testing

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,7 +2,6 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,13 +11,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.JavaJokes;
-import com.example.jon.myapplication.backend.myApi.MyApi;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-
-import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,9 +19,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,88 +30,29 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
             return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     public void tellJavaJoke(View view){
-
         JavaJokes javaJoke = new JavaJokes();
         String joke = javaJoke.getJoke(getString(R.string.java_key));
-
         Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
     public void tellAndroidJoke(View view){
-
         JavaJokes javaJoke = new JavaJokes();
         String joke = javaJoke.getJoke(getString(R.string.android_key));
-
         Intent intent = new Intent(this, com.example.androidjokelib.JokeActivity.class);
         intent.putExtra("android_joke", joke);
         startActivity(intent);
-
-
     }
 
     public void tellGCEJoke(View view){
-
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
-
+        new EndpointsAsyncTask(this).execute(new Pair<Context, String>(this, "Manfred"));
     }
 
-
-    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-        private  MyApi myApiService = null;
-        private Context context;
-
-        @Override
-        protected String doInBackground(Pair<Context, String>... params) {
-            if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
-
-                myApiService = builder.build();
-            }
-
-            context = params[0].first;
-            String name = params[0].second;
-
-            try {
-                return myApiService.sayHi(name).execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            Intent intent = new Intent(getApplicationContext(), com.example.androidjokelib.JokeActivity.class);
-            intent.putExtra("android_joke", result);
-            startActivity(intent);
-
-        }
-    }
 
 }
