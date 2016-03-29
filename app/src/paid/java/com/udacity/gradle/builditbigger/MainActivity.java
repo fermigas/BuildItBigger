6238e00da4e377baa7dd37bd937fa5671d0397aa
jon <newbluetwo@gmail.com>
2016-03-29 16:14:24 -0400

    add free and paid flavors, removing ads from paid
    This completes step 5:
    
    Step 5: Add a Paid Flavor
    
    Add free and paid product flavors to your app. Remove the ad (and any dependencies you can)
    from the paid flavor.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,58 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.util.Pair;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.JavaJokes;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJavaJoke(View view){
+        JavaJokes javaJoke = new JavaJokes();
+        String joke = javaJoke.getJoke(getString(R.string.java_key));
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+    }
+
+    public void tellAndroidJoke(View view){
+        JavaJokes javaJoke = new JavaJokes();
+        String joke = javaJoke.getJoke(getString(R.string.android_key));
+        Intent intent = new Intent(this, com.example.androidjokelib.JokeActivity.class);
+        intent.putExtra("android_joke", joke);
+        startActivity(intent);
+    }
+
+    public void tellGCEJoke(View view){
+        new EndpointsAsyncTask(this).execute();
+    }
+
+
+}
